b475a843e5ef077affb72816f5abc7c3e3c7fd2c
Brett Zamir <brettz9@yahoo.com>
2013-08-02 14:56:03 +0800

    Update destructure.js

--- destructure.js
+++ destructure.js
@@ -5,7 +5,7 @@
 * @param {function} unpackCb The callback which will be passed each value of the array in order
 * @returns {*} Returns the result of the unpackCb() execution
 * @example
-* var destructure = require('destructure');
+* var destructure = require('destructure'); // Omit this line if including the destructure.js file in a browser
 * var arr = [3, 4, 5];
 * var added = destructure(arr, function (a, b, c) {
 *     return a + b + c;
